399c0641c78b6785c0969b363405071a545fbe43
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-12 12:10:04 +0500

    js line 15
    continue

--- loop/app.js
+++ loop/app.js
@@ -1,10 +1,40 @@
 
 // Qno :- 01) What is the syntax of a basic for loop in JavaScript?
 
-// let num = 10
-// for(let i = 1; i<=num; i++){
-//     console.log(num);
+
+// for(let i = 0; i<=10; i++){
+
 // }
 
 //  Qno :- 02)   How do you initialize a for loop variable?
 
+// let num = 10
+// for(let i=0; i<num; i++){
+//     console.log(num);
+// }
+
+// Qno :- 03) What is the purpose of the condition in a for loop?
+
+// Qno :- 04) How do you increment or decrement a loop variable in a for loop?
+
+// Qno :- 05) Write a for loop that prints numbers from 1 to 10.
+
+// Qno :- 06) How do you create an infinite loop using the for loop?
+
+// Qno :- 07) How do you use a for loop to iterate over an array?
+
+// Qno :- 08) Write a for loop that prints the elements of an array.
+
+// Qno :- 09) What will the following for loop print?
+
+// Qno :- 10) How can you break out of a for loop prematurely?
+
+// Qno :- 11) How do you skip the current iteration in a for loop?
+
+// Qno :- 12) Write a for loop that prints only the even numbers from 1 to 10.
+
+// Qno:- 13) What happens if the condition in a for loop is always false?
+
+// Qno:- 14) How can you use a for loop to iterate over a string?
+
+// Qno:- 15) Write a for loop that prints each character of a string.
