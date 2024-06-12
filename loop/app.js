1c39f0d764f36a9bdf433ade365319a465608248
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-12 11:59:15 +0500

    js line 7
    continue

---
+++ loop/app.js
@@ -0,0 +1,10 @@
+
+// Qno :- 01) What is the syntax of a basic for loop in JavaScript?
+
+// let num = 10
+// for(let i = 1; i<=num; i++){
+//     console.log(num);
+// }
+
+//  Qno :- 02)   How do you initialize a for loop variable?
+
