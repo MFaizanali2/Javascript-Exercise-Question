4024c75cf0f1830a91d868921bf15108a4adbcc8
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-19 23:42:34 +0500

    line 9
    continue

---
+++ dom/app.js
@@ -0,0 +1,9 @@
+    // BASIC QUESTION
+
+// Qno:- 01) What is the DOM in JavaScript?
+
+// Qno:- 02) How do you access the DOM in JavaScript?
+
+// Qno:- 03) What is the purpose of document.getElementById()?
+
+// Qno:- 04) How do you use document.getElementsByClassName()?
