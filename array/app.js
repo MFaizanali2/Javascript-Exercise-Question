49d5f3c62da72b6521171561b32401b256ba8aa3
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-21 12:36:51 +0500

    js line 45
    continue

--- array/app.js
+++ array/app.js
@@ -2,19 +2,47 @@
 
 // Qno:- 01) What is an array in JavaScript?
 
+// let arr = [];
+// array have store many data
+
 // Qno:- 02) How do you create an array in JavaScript?
+
+// let array = ["mango" , "apple", "banana", "liche"]
+// console.log(array)
 
 // Qno:- 03) How do you access the first element of an array?
 
+// let array = ["mango" , "apple", "banana", "liche"];
+// console.log(array[0])
+
 // Qno:- 04) How do you access the last element of an array?
+
+// let array = ["mango" , "apple", "banana", "liche"];
+// console.log(array[array.length -1])
 
 // Qno:- 05) How do you add an element to the end of an array?
 
+// let array = ["mango" , "apple", "banana", "liche"];
+// array.push("kiwi");
+// console.log(array)
+
 // Qno:- 06) How do you remove the last element from an array?
+
+// let array = ["mango" , "apple", "banana", "liche"];
+// array.pop();
+// console.log(array)
 
 // Qno:- 07) How do you add an element to the beginning of an array?
 
+// let array = ["mango" , "apple", "banana", "liche"];
+// array.unshift("kiwi");
+// console.log(array)
+
 // Qno:- 08) How do you remove the first element from an array?
+
+// let array = ["mango" , "apple", "banana", "liche"];
+// array.shift();
+// console.log(array)
 
 // Qno:- 09) How can you find the length of an array?
 
@@ -28,4 +56,4 @@
 
 // Qno:- 14) How do you concatenate two arrays?
 
-// Qno:- 15) How do you create an empty array?
+// Qno:- 15) How do you create an empty array?  
