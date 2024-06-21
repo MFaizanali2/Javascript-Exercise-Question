9f180c571d9203beac0d951cedf13e641c1d3d7f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-21 11:39:55 +0500

    js line 31
    continue

---
+++ array/app.js
@@ -0,0 +1,31 @@
+        // BASIC QUESTION
+
+// Qno:- 01) What is an array in JavaScript?
+
+// Qno:- 02) How do you create an array in JavaScript?
+
+// Qno:- 03) How do you access the first element of an array?
+
+// Qno:- 04) How do you access the last element of an array?
+
+// Qno:- 05) How do you add an element to the end of an array?
+
+// Qno:- 06) How do you remove the last element from an array?
+
+// Qno:- 07) How do you add an element to the beginning of an array?
+
+// Qno:- 08) How do you remove the first element from an array?
+
+// Qno:- 09) How can you find the length of an array?
+
+// Qno:- 10) Write code to iterate over an array and print each element?
+
+// Qno:- 11) How do you check if a value is an array in JavaScript?
+
+// Qno:- 12) What is the difference between push() and unshift() methods?
+
+// Qno:- 13) What is the difference between pop() and shift() methods?
+
+// Qno:- 14) How do you concatenate two arrays?
+
+// Qno:- 15) How do you create an empty array?
