3ee8eac58a89816239de4f2f645e2a4ebcf8ad5f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-04 23:31:41 +0500

    Update app.js

---
+++ function/app.js
@@ -0,0 +1,32 @@
+        // Basic Questions :-
+
+Qno:- 01) 
+
+Qno:- 02)
+
+Qno:- 03)
+
+Qno:- 04)
+
+Qno:- 05)
+
+Qno:- 06)
+
+Qno:- 07)
+
+Qno:- 08)
+
+Qno:- 09)
+
+Qno:- 10)
+
+Qno:- 11)
+
+Qno:- 12)
+
+Qno:- 13)
+
+Qno:- 14)
+
+Qno:- 15)
+
