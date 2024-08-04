a9c6ce62233c225d06d130876191e0c2d9e972ef
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-04 23:35:30 +0500

    Update app.js

--- function/app.js
+++ function/app.js
@@ -1,32 +1,32 @@
         // Basic Questions :-
 
-Qno:- 01) 
+// Qno:- 01) What is a function in JavaScript?
 
-Qno:- 02)
+// Qno:- 02) How do you define a function in JavaScript?
 
-Qno:- 03)
+// Qno:- 03) How do you call a function in JavaScript?
 
-Qno:- 04)
+// Qno:- 04) What is the difference between a function declaration and a function expression?
 
-Qno:- 05)
+// Qno:- 05) What are parameters in a function?
 
-Qno:- 06)
+// Qno:- 06) What are arguments in a function?
 
-Qno:- 07)
+// Qno:- 07) How do you return a value from a function?
 
-Qno:- 08)
+// Qno:- 08) Can a function return multiple values? If so, how?
 
-Qno:- 09)
+// Qno:- 09) What is an anonymous function?
 
-Qno:- 10)
+// Qno:- 10) What is a named function?
 
-Qno:- 11)
+// Qno:- 11) How do you define a default parameter in a function?
 
-Qno:- 12)
+// Qno:- 12) How do you create a function that takes a variable number of arguments?
 
-Qno:- 13)
+// Qno:- 13) What is the arguments object in a function?
 
-Qno:- 14)
+// Qno:- 14) How do you pass an array as an argument to a function?
 
-Qno:- 15)
+// Qno:- 15) How do you pass an object as an argument to a function?
 
