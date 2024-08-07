bef539df549f0cd424106a0abc85f98199403443
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-07 20:01:56 +0500

    Update app.js

--- array/app.js
+++ array/app.js
@@ -1,95 +1,95 @@
-        // BASIC QUESTION
+        BASIC QUESTION
 
-// Qno:- 01) What is an array in JavaScript?
+Qno:- 01) What is an array in JavaScript?
 
-// let arr = [];
-// array have store many data
+let arr = [];
+array have store many data
 
-// Qno:- 02) How do you create an array in JavaScript?
+Qno:- 02) How do you create an array in JavaScript?
 
-// let array = ["mango" , "apple", "banana", "liche"]
-// console.log(array)
+let array = ["mango" , "apple", "banana", "liche"]
+console.log(array)
 
-// Qno:- 03) How do you access the first element of an array?
+Qno:- 03) How do you access the first element of an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// console.log(array[0])
+let array = ["mango" , "apple", "banana", "liche"];
+console.log(array[0])
 
-// Qno:- 04) How do you access the last element of an array?
+Qno:- 04) How do you access the last element of an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// console.log(array[array.length -1])
+let array = ["mango" , "apple", "banana", "liche"];
+console.log(array[array.length -1])
 
-// Qno:- 05) How do you add an element to the end of an array?
+Qno:- 05) How do you add an element to the end of an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// array.push("kiwi");
-// console.log(array)
+let array = ["mango" , "apple", "banana", "liche"];
+array.push("kiwi");
+console.log(array)
 
-// Qno:- 06) How do you remove the last element from an array?
+Qno:- 06) How do you remove the last element from an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// array.pop();
-// console.log(array)
+let array = ["mango" , "apple", "banana", "liche"];
+array.pop();
+console.log(array)
 
-// Qno:- 07) How do you add an element to the beginning of an array?
+Qno:- 07) How do you add an element to the beginning of an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// array.unshift("kiwi");
-// console.log(array)
+let array = ["mango" , "apple", "banana", "liche"];
+array.unshift("kiwi");
+console.log(array)
 
-// Qno:- 08) How do you remove the first element from an array?
+Qno:- 08) How do you remove the first element from an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// array.shift();
-// console.log(array)
+let array = ["mango" , "apple", "banana", "liche"];
+array.shift();
+console.log(array)
 
-// Qno:- 09) How can you find the length of an array?
+Qno:- 09) How can you find the length of an array?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// console.log(array.length)
+let array = ["mango" , "apple", "banana", "liche"];
+console.log(array.length)
 
-// Qno:- 10) Write code to iterate over an array and print each element?
+Qno:- 10) Write code to iterate over an array and print each element?
 
-// let array = ["mango" , "apple", "banana", "liche"];
-// for(let i=0; i<=array.length; i++){
-//     console.log(array[i])
-// }
+let array = ["mango" , "apple", "banana", "liche"];
+for(let i=0; i<=array.length; i++){
+    console.log(array[i])
+}
 
-// Qno:- 11) How do you check if a value is an array in JavaScript?
-// let array = ["karachi", "lahore", "multan"];
-// console.log(array[0])
-
-
-// Qno:- 12) What is the difference between push() and unshift() methods?
-// let array = ["karachi", "lahore", "multan"];
-// console.log(array)
-// array.push("quetta");  push add last element of array
-// array.unshift("quetta") unshift add first element of an array
-// console.log(array)
-
-// Qno:- 13) What is the difference between pop() and shift() methods?
-// let array = ["karachi", "lahore", "multan"];
-// console.log(array);
-// array.pop();  pop remove last element of array
-// console.log(array);
-
-// let array1 = ["karachi", "lahore", "multan"];
-// console.log(array1);
-// array1.shift(); shift remove first element of array
-// console.log(array1);
+Qno:- 11) How do you check if a value is an array in JavaScript?
+let array = ["karachi", "lahore", "multan"];
+console.log(array[0])
 
 
-// Qno:- 14) How do you concatenate two arrays?
-// let fruit = ["apple", "mango", "banana", "kiwi"];
-// console.log(fruit);
-// let vegetable = ["carrot", "potato", "tomato", "onion"];
-// console.log(vegetable)
+Qno:- 12) What is the difference between push() and unshift() methods?
+let array = ["karachi", "lahore", "multan"];
+console.log(array)
+array.push("quetta");  push add last element of array
+array.unshift("quetta") unshift add first element of an array
+console.log(array)
 
-// let fruitsAndVegetables = fruit.concat(vegetable);
-// console.log(fruitsAndVegetables);
+Qno:- 13) What is the difference between pop() and shift() methods?
+let array = ["karachi", "lahore", "multan"];
+console.log(array);
+array.pop();  pop remove last element of array
+console.log(array);
+
+let array1 = ["karachi", "lahore", "multan"];
+console.log(array1);
+array1.shift(); shift remove first element of array
+console.log(array1);
 
 
-// Qno:- 15) How do you create an empty array?  
-// let array = [];
-// console.log(array)
+Qno:- 14) How do you concatenate two arrays?
+let fruit = ["apple", "mango", "banana", "kiwi"];
+console.log(fruit);
+let vegetable = ["carrot", "potato", "tomato", "onion"];
+console.log(vegetable)
+
+let fruitsAndVegetables = fruit.concat(vegetable);
+console.log(fruitsAndVegetables);
+
+
+Qno:- 15) How do you create an empty array?  
+let array = [];
+console.log(array)
